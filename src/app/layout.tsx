b5fed048c818430a357f50189622cea54608d85b
lho3ein <Khalili.h8181@gmail.com>
2026-09-14 20:30:06 +0330

    change defauld theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <QueryProvider>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="light"
               enableSystem
               disableTransitionOnChange
             >
